616d73a18bfd8216fd3be0420a669ff055f891fb
Mary Mott <mott.maryt@gmail.com>
2015-08-19 22:37:28 -0700

    jQueryfied and IT WORKS

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,17 +1,27 @@
-(function() {
-  var playerChoiceDiv = document.getElementById('player-choice');
-  var gameDataDiv = document.getElementById('game-data');
-  var winnerDiv = document.getElementById('winner');
-  var reset = document.getElementsByClassName('reset')[0];
+$(function() {
+  var $playerChoiceDiv = $('#player-choice');
+  var $gameDataDiv = $('#game-data');
+  var $winnerDiv = $('#winner');
+  var $reset = $('.reset').first();
+  var $wonNode = $('#won');
+  var $lostNode = $('#lost');
+  var $tiedNode = $('#ties');
+  var $matchesNode = $('#matches');
 
 
-  function playGame(e) {
-    var player = e.target.getAttribute('data-choice');
+  function playGame(evt) {
+    var $evt = $(evt.target);
+    var $player = $evt.attr('data-choice');
     var opponent = opponentChoice();
 
-    e.stopPropagation();
-    choicesText(player, opponent);
-    getWinner(player, opponent);
+    if ($evt.is('.image-holder')) {
+      //get the value of the child data-choice
+      $player = $evt.children().attr('data-choice');
+    }
+
+    evt.stopPropagation();
+    choicesText($player, opponent);
+    getWinner($player, opponent);
   }
 
 
@@ -30,7 +40,7 @@
 
 
   function choicesText(player, opponent) {
-   gameDataDiv.innerHTML = 'Player chose ' +  player + '. Opponent chose ' + opponent + '.';
+   $gameDataDiv.html('Player chose ' +  player + '. Opponent chose ' + opponent + '.');
   }
 
 
@@ -38,40 +48,32 @@
     var playerWins1 = player === 'rock' && opponent === 'scissors';
     var playerWins2 = player === 'paper' && opponent === 'rock';
     var playerWins3 = player === 'scissors' && opponent === 'paper';
-    var wonNode = document.getElementById('won');
-    var lostNode = document.getElementById('lost');
-    var tiedNode = document.getElementById('ties');
-    var matchesNode = document.getElementById('matches');
 
-    matchesNode.innerHTML++;
+    $matchesNode.html(parseInt($($matchesNode).html(), 10)+1);
+    //10 is wtf radix value??...also, thank you stackoverflow for this parseInt tip w/ .html()
 
     if (player === opponent) {
-      tiedNode.innerHTML++;
-      winnerDiv.innerHTML = 'The result is a tie!';
+      $tiedNode.html(parseInt($($tiedNode).html(), 10)+1);
+      $winnerDiv.html('The result is a tie!');
     } else if (playerWins1 || playerWins2 || playerWins3) {
-      wonNode.innerHTML++;
-      winnerDiv.innerHTML = 'You win!';
+      $wonNode.html(parseInt($($wonNode).html(), 10)+1);
+      $winnerDiv.html('You win!');
     } else {
-      lostNode.innerHTML++;
-      winnerDiv.innerHTML = 'Sorry, your opponent won.';
+      $lostNode.html(parseInt($($lostNode).html(), 10)+1);
+      $winnerDiv.html('Sorry, your opponent won.');
     }
   }
 
   function resetScores() {
-    var wonNode = document.getElementById('won');
-    var lostNode = document.getElementById('lost');
-    var tiedNode = document.getElementById('ties');
-    var matchesNode = document.getElementById('matches');
-
-    gameDataDiv.innerHTML = '';
-    winnerDiv.innerHTML = '';
-    matchesNode.innerHTML = 0;
-    tiedNode.innerHTML = 0;
-    wonNode.innerHTML = 0;
-    lostNode.innerHTML = 0;
+    $gameDataDiv.html('');
+    $winnerDiv.html('');
+    $matchesNode.html(0);
+    $tiedNode.html(0);
+    $wonNode.html(0);
+    $lostNode.html(0);
   }
 
-  playerChoiceDiv.addEventListener('click', playGame);
-  reset.addEventListener('click', resetScores);
+  $playerChoiceDiv.on('click', playGame);
+  $reset.on('click', resetScores);
 
-})();
+});
